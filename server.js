d947078b4e4558f2315aa2af10704936ed74c4c8
Feinbube <frank@feinbube.de>
2014-02-17 14:26:06 +0000

    fixed db-handling :)
    git-svn-id: svn+ssh://code.hpi.uni-potsdam.de/svn@119 f9a22ca8-cb78-4b93-9e7d-cb4c118d92a2

--- server.js
+++ server.js
@@ -1,61 +1,59 @@
+var assert = require('assert');
+var http = require('http');
+var fs = require('fs');
+var dbengine = require('tingodb')()
+
 function question(ID, text, answers, correctAnswer) {
-	this.ID = ID;
-	this.text = text;
-	this.answers = answers;
-	this.correctAnswer = correctAnswer;
+    this.ID = ID;
+    this.text = text;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
 }
 
-function checkResult(questionID, answerIndex){
-	for (var i = 0; i < questions.length; ++i) {
-		if (questions[i].ID == questionID) {
-			return answerIndex == questions[i].correctAnswer;
-		}
-	}
-    return false;
+// ------------------------------------------------------------------------- //
+//                            database stuff                                 //
+// ------------------------------------------------------------------------- //
+
+var db = new dbengine.Db('./db', {});
+var collection = db.collection("quiz_db");
+
+function fillDB() {        
+    var questions = new Array();
+    questions[0] = new question(0, "What is 5+5?",  ["10", "11", "12", "Dogge"], 0);
+    questions[1] = new question(1, "What can dogs not do?",  ["Look up!", "Cheat!", "Your Mother!", "B Cutzz :D"], 0);
+    questions[2] = new question(2, "What's in it for me?",  ["Dunno", "Your Mother!", "Chocolate?", "Beer"], 0);
+    questions[3] = new question(3, "What let the dogs out?",  ["You", "U", "OO", "Who!"], 3);
+
+    questions.forEach(function(question){
+        collection.insert(questionToDocument(question));
+    });
+}
+
+collection.findOne({ID: 0}, function(err, item) {
+    if(item == null) {
+        fillDB();
+    }
+});
+
+function loadQuestion(id, callback) {
+    collection.findOne({ID: id}, function(err, item) {
+        callback(documentToQuestion(item));
+     })
 }
 
 function questionToDocument(question) {
     return {ID: question.ID, text: question.text, answers: question.answers, correctAnswer: question.correctAnswer};
 }
 
-function checkError(err) {
-  if (err != null)
-    console.log(err);
-  assert.equal(null, err);
+function documentToQuestion(doc) {
+    return new question(doc.ID, doc.text, doc.answers, doc.correctAnswer);
 }
 
-// save data in database
-var Db = require('tingodb')().Db;
-var assert = require('assert');
-
-var db = new Db('./db', {});
-var collection = db.collection("quiz_db");
-
-var questions = new Array();
-questions[0] = new question(0, "What is 5+5?",  ["10", "11", "12", "Dogge"], 0);
-questions[1] = new question(1, "What can dogs not do?",  ["Look up!", "Cheat!", "Your Mother!", "B Cutzz :D"], 0);
-questions[2] = new question(2, "What's in it for me?",  ["Dunno", "Your Mother!", "Chocolate?", "Beer"], 0);
-questions[3] = new question(3, "What let the dogs out?",  ["You", "U", "OO", "Who!"], 3);
-
-questions.forEach(function(question){
-    collection.save(questionToDocument(question), checkError);
-});
-
-function loadQuestions(collection) {
-    dbquestions = new Array();
-    var cursor = collection.find({ID:0}, function(err, doc){
-        checkError(err);
-        doc.each(function(entry){
-            console.log(entry.ID);
-            dbquestions.push("...");
-        });
-    });
-    return dbquestions;
-}
+// ------------------------------------------------------------------------- //
+//                     server communication stuff                            //
+// ------------------------------------------------------------------------- //
 
 // start up server
-var http = require('http');
-var fs = require('fs');
 var app = http.createServer(function (request, response) {
     fs.readFile("client.html", 'utf-8', function (error, data) {
         response.writeHead(200, {'Content-Type': 'text/html'});
@@ -64,22 +62,19 @@ var app = http.createServer(function (request, response) {
     });
 }).listen(1337);
 
-questions = loadQuestions(collection);
-
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function(socket) {
+
     socket.on('get_next_question', function(data) {
-    	var index = data['currentQuestion'];
-    	if (index >= questions.length) return; // TODO error message
-    	var current = questions[index];
-        io.sockets.emit("new_question", { question: current.text, answers: current.answers });
+        loadQuestion(data['currentQuestion'], function(current) {
+            io.sockets.emit("new_question", { question: current.text, answers: current.answers });
+        });
     });
 
     socket.on('submit_answer', function(data) {
-    	var answerIndex = data['answer'];
-        var questionID = data['questionID'];
-    	var correct = checkResult(questionID, answerIndex);
-        io.sockets.emit("result", { result: correct ? 1 : 0});
+        loadQuestion(data['questionID'], function(current) {
+            io.sockets.emit("result", { result: current.correctAnswer == data['answer'] ? 1 : 0});
+        });
     });
 });

4b9b681c9d2b9f7058411db4c9fbad989bbc0daa
Feinbube <frank@feinbube.de>
2014-05-10 11:45:35 +0000

    ++
    git-svn-id: svn+ssh://code.hpi.uni-potsdam.de/svn@169 f9a22ca8-cb78-4b93-9e7d-cb4c118d92a2

---
+++ client/tests.js
@@ -0,0 +1,28 @@
+function runSelfTest() {
+    testRegistration();
+    testLogin();
+    testQuestions();
+    testScores();
+}
+
+function testRegistration() {
+    console.log("Running Registration Test.")
+    // tryToRegisterUser()
+}
+
+function testLogin() {
+    console.log("Running Login Test.") 
+    // tryToLoginUser()   
+    // logoutUser()
+}
+
+function testQuestions() {
+    console.log("Running Question Test.")  
+    // showNewQuestion()
+    // submitAnswer(0)
+}
+
+function testScores() {
+   console.log("Running Score Test.")  
+   // loadHighscores()
+}
